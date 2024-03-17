c1ca1a38cbdef0a2cd32ce2ecbf2bd3a78e7a95a
Nisha Ramprasath <115043583+nisharampra@users.noreply.github.com>
2024-03-17 22:36:09 +0800

    Add files via upload

---
+++ expressions/src/expressions/expressions.java
@@ -0,0 +1,22 @@
+package expressions;
+
+public class expressions {
+    public static void main(String[] args) {
+
+ int friends=10;
+ friends=friends -1;
+ 
+ int blu=10;
+ blu=blu *2;
+ 
+ int div=10;
+ div=div/2;
+ 
+ int per=10;
+ per=per%2;
+ System.out.println(friends);
+ System.out.println(blu);
+ System.out.println(div);
+ System.out.println(per);
+    }
+}
